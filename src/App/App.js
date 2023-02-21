91e1a341a156fe51228728b2a1db604b856709f2
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-21 09:47:10 -0700

    Add an all reservation component and fetch reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Reservation from '../reservations/Reservation';
 
 function App() {
 	const [reservations, setReservations] = useState([])
-
 	useEffect(() => {
-		setReservations()
+		fetch('http://localhost:3001/api/v1/reservations')
+			.then(response => {
+				if (response.ok) {
+					return response.json()
+				}
+			})
+			.then(response => {
+				console.log(response)
+				setReservations(response)
+			})
 	}, [])
-    return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+	return (
+		<div className="App">
+			<h1 className='app-title'>Turing Cafe Reservations</h1>
+			<div className='resy-form'>
 
-        </div>
-        <div className='resy-container'>
-          
-        </div>
-      </div>
-    )
+			</div>
+			<div className='resy-container'>
+				<Reservation reservation={reservations}/>
+			</div>
+		</div>
+	)
 }
 
 export default App;
---
+++ src/reservations/Reservation.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+export default function Reservation({reservation}) {
+	console.log(reservation)
+	return (
+		<section>
+
+		</section>
+	)
+}
+
+// {
+// 	id: 1,
+// 	name: "Christie",
+// 	date: "12/29",
+// 	time: "7:00",
+// 	number: 12
+// 	},
